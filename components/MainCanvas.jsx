2a3edea538ae28f41c2e35c770a9468046eb800e
Rasheednour <rasheednour@gmail.com>
2023-08-11 23:26:04 +0200

    FEAT: Optimized 3d model further

--- components/MainCanvas.jsx
+++ components/MainCanvas.jsx
@@ -2,12 +2,46 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { World } from "./World";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls , Stars, Sky, Environment} from "@react-three/drei";
 const MainCanvas = () => {
   return (
     <Suspense fallback={null}>
       <Canvas onCreated={({ gl }) => {
      window.renderer = gl;}}>
+      <spotLight
+            position={[0, 2, 0]}
+            distance={10}
+            intensity={12}
+            angle={0.65}
+            color="white"
+            penumbra={1.9}
+          />
+          <directionalLight
+            name="skylight"
+            intensity={2.5}
+            decay={3}
+            color="#6f8fff"
+            position={[0.095, 9.303, 0.144]}
+            rotation={[-1.303, 0.278, 2.143]}
+            castShadow
+          ></directionalLight>
+          <Sky
+              distance={1000}
+              sunPosition={[0,0,0]}
+              inclination={0}
+              azimuth={0.25}
+            />
+            <Stars
+              radius={100}
+              depth={50}
+              count={5000}
+              factor={4}
+              saturation={1}
+              fade
+              speed={1}
+            />
+            <Environment preset="dawn" />
+       
         <World />
         <OrbitControls/>
       </Canvas>
--- components/World.jsx
+++ components/World.jsx
@@ -5,11 +5,14 @@ Command: npx gltfjsx@6.2.10 public/3d/world.glb
 
 import React, { useRef, useEffect } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
-
+import * as THREE from 'three';
 export function World(props) {
   const group = useRef()
   const { nodes, materials, animations } = useGLTF('/3d/world.glb')
   const { actions } = useAnimations(animations, group)
+  const waterMaterial = new THREE.MeshStandardMaterial({
+    color: 'cyan'
+});
   useEffect(() => {
     Object.values(actions).forEach((action) => {
       action.play();
@@ -18,7 +21,8 @@ export function World(props) {
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
-        <pointLight name="bonfire_light" intensity={1.514} decay={2} color="#ff7c2f" position={[-1.164, -0.769, 1.373]} rotation={[-Math.PI / 2, 0, Math.PI / 2]} />
+        <group name="posters" position={[0.054, 0.177, -0.396]} rotation={[Math.PI / 2, 0, -Math.PI / 2]} scale={[0.168, 1, 0.233]} />
+        <group name="walls&floor001" position={[0, -0.476, 0]} rotation={[0, 1.571, 0]} />
         <mesh name="rocks" geometry={nodes.rocks.geometry} material={materials.rock} position={[-2.83, -0.88, -2.03]} rotation={[Math.PI, -1.426, Math.PI]} scale={[0.335, 0.366, 0.353]} />
         <group name="plants" position={[0.808, -0.745, 2.11]}>
           <mesh name="NurbsPath013" geometry={nodes.NurbsPath013.geometry} material={materials.green_plant} />
@@ -121,7 +125,9 @@ export function World(props) {
           <mesh name="NurbsPath013_1" geometry={nodes.NurbsPath013_1.geometry} material={materials.rose_emissive} />
         </group>
         <mesh name="earth" geometry={nodes.earth.geometry} material={materials.ground} position={[-4.452, -0.956, -3.564]} scale={[26.298, 1, 27.06]} />
-        <mesh name="lake" geometry={nodes.lake.geometry} material={materials.water} position={[-5.511, -1.75, -3.538]} scale={[2.834, 3.904, 2.472]} />
+        <mesh name="lake" geometry={nodes.lake.geometry} material={waterMaterial} position={[-5.511, -1.75, -3.538]} scale={[2.834, 3.904, 2.472]}>
+
+        </mesh>
         <group name="tree_13001" position={[-2.473, -0.771, 1.171]}>
           <mesh name="Sphere001" geometry={nodes.Sphere001.geometry} material={materials.tree_bush} />
           <mesh name="Sphere001_1" geometry={nodes.Sphere001_1.geometry} material={materials.tree_trunk} />
@@ -171,13 +177,10 @@ export function World(props) {
           <mesh name="Sphere001_1" geometry={nodes.Sphere001_1.geometry} material={materials.tree_trunk} />
         </group>
         <group name="avatar" position={[-0.504, -0.228, 0.546]} rotation={[-0.049, 0.408, 0.277]} scale={[1, 0.942, 1]}>
-          <mesh name="Plane005" geometry={nodes.Plane005.geometry} material={materials['black tshirt']} />
-          <mesh name="Plane005_1" geometry={nodes.Plane005_1.geometry} material={materials.hair_black} />
-          <mesh name="Plane005_2" geometry={nodes.Plane005_2.geometry} material={materials.skin} />
-          <mesh name="Plane005_3" geometry={nodes.Plane005_3.geometry} material={materials.hair_grey} />
-          <mesh name="Plane005_4" geometry={nodes.Plane005_4.geometry} material={materials.bronze} />
-          <mesh name="Plane005_5" geometry={nodes.Plane005_5.geometry} material={materials.arch_glass} />
-          <mesh name="Plane005_6" geometry={nodes.Plane005_6.geometry} material={materials.grey_dark} />
+          <mesh name="Plane005" geometry={nodes.Plane005.geometry} material={materials.hair_black} />
+          <mesh name="Plane005_1" geometry={nodes.Plane005_1.geometry} material={materials.skin} />
+          <mesh name="Plane005_2" geometry={nodes.Plane005_2.geometry} material={materials.hair_grey} />
+          <mesh name="Plane005_3" geometry={nodes.Plane005_3.geometry} material={materials.bronze} />
         </group>
         <group name="book_shelves" position={[0.014, 0.092, -0.608]} rotation={[0, 1.571, 0]}>
           <mesh name="Cube036" geometry={nodes.Cube036.geometry} material={materials.white} />
@@ -187,20 +190,13 @@ export function World(props) {
           <mesh name="Cube036_4" geometry={nodes.Cube036_4.geometry} material={materials.two_towers} />
           <mesh name="Cube036_5" geometry={nodes.Cube036_5.geometry} material={materials.forest} />
           <mesh name="Cube036_6" geometry={nodes.Cube036_6.geometry} material={materials.sorcery} />
-          <mesh name="Cube036_7" geometry={nodes.Cube036_7.geometry} material={materials.brown_wood} />
-          <mesh name="Cube036_8" geometry={nodes.Cube036_8.geometry} material={materials.window_frame} />
         </group>
         <group name="chair" position={[-0.396, -0.511, 0.511]} rotation={[Math.PI / 2, 0, -0.525]} scale={0.252}>
           <mesh name="Cylinder090" geometry={nodes.Cylinder090.geometry} material={materials.smooth_plastic_grey} />
           <mesh name="Cylinder090_1" geometry={nodes.Cylinder090_1.geometry} material={materials.metal_black} />
           <mesh name="Cylinder090_2" geometry={nodes.Cylinder090_2.geometry} material={materials.brown_wood} />
         </group>
-        <group name="walls&floor" position={[0, -0.476, 0]} rotation={[0, 1.571, 0]}>
-          <mesh name="Plane" geometry={nodes.Plane.geometry} material={materials.carpet} />
-          <mesh name="Plane_1" geometry={nodes.Plane_1.geometry} material={materials.wall_material} />
-          <mesh name="Plane_2" geometry={nodes.Plane_2.geometry} material={materials.window_frame} />
-          <mesh name="Plane_3" geometry={nodes.Plane_3.geometry} material={materials.arch_glass} />
-        </group>
+        <mesh name="walls&floor" geometry={nodes['walls&floor'].geometry} material={materials.wall_material} position={[0, -0.476, 0]} rotation={[0, 1.571, 0]} />
         <group name="lamps" position={[-0.081, -0.125, 0.042]} rotation={[-1.484, 1.571, 0]} scale={0.041}>
           <mesh name="Sphere003" geometry={nodes.Sphere003.geometry} material={materials.emission} />
           <mesh name="Sphere003_1" geometry={nodes.Sphere003_1.geometry} material={materials.white_glass} />
@@ -215,7 +211,6 @@ export function World(props) {
           <mesh name="Cube193_6" geometry={nodes.Cube193_6.geometry} material={materials.rough_plastic_dark_grey} />
           <mesh name="Cube193_7" geometry={nodes.Cube193_7.geometry} material={materials.red_light} />
           <mesh name="Cube193_8" geometry={nodes.Cube193_8.geometry} material={materials.rough_plastic_white_logo} />
-          <mesh name="Cube193_9" geometry={nodes.Cube193_9.geometry} material={materials.NES_LOGO} />
         </group>
         <mesh name="pc_monitor" geometry={nodes.pc_monitor.geometry} material={nodes.pc_monitor.material} position={[0.002, 0.086, -0.3]} />
         <group name="office_plants" position={[-0.739, -0.068, 0.22]} rotation={[0, 1.571, 0]} scale={0.647}>
@@ -233,30 +228,11 @@ export function World(props) {
           <mesh name="Cube004_1" geometry={nodes.Cube004_1.geometry} material={materials.earth} />
           <mesh name="Cube004_2" geometry={nodes.Cube004_2.geometry} material={materials.green_plant} />
         </group>
-        <group name="posters" position={[0.054, 0.177, -0.396]} rotation={[Math.PI / 2, 0, -Math.PI / 2]} scale={[0.168, 1, 0.233]}>
-          <mesh name="Plane002" geometry={nodes.Plane002.geometry} material={materials.et} />
-          <mesh name="Plane002_1" geometry={nodes.Plane002_1.geometry} material={materials.final_fantasy} />
-          <mesh name="Plane002_2" geometry={nodes.Plane002_2.geometry} material={materials.b2f} />
-          <mesh name="Plane002_3" geometry={nodes.Plane002_3.geometry} material={materials.monkey_island} />
-          <mesh name="Plane002_4" geometry={nodes.Plane002_4.geometry} material={materials.mega_man} />
-        </group>
         <group name="pc&floppies" position={[0.115, 0.014, -0.371]}>
-          <mesh name="Plane015" geometry={nodes.Plane015.geometry} material={materials.white_striped_paper} />
-          <mesh name="Plane015_1" geometry={nodes.Plane015_1.geometry} material={materials.floppy_disk_black} />
-          <mesh name="Plane015_2" geometry={nodes.Plane015_2.geometry} material={materials.silver_metal} />
-          <mesh name="Plane015_3" geometry={nodes.Plane015_3.geometry} material={materials.floppy_disk_blue} />
-          <mesh name="Plane015_4" geometry={nodes.Plane015_4.geometry} material={materials.rough_plastic_beige} />
-          <mesh name="Plane015_5" geometry={nodes.Plane015_5.geometry} material={materials.rough_plastic_dark_beige} />
-          <mesh name="Plane015_6" geometry={nodes.Plane015_6.geometry} material={materials.rough_plastic_GREY} />
-          <mesh name="Plane015_7" geometry={nodes.Plane015_7.geometry} material={materials.green_emission} />
-          <mesh name="Plane015_8" geometry={nodes.Plane015_8.geometry} material={materials.metalic_light_grey} />
-          <mesh name="Plane015_9" geometry={nodes.Plane015_9.geometry} material={materials.grey_dark} />
-          <mesh name="Plane015_10" geometry={nodes.Plane015_10.geometry} material={materials.smooth_plastic_grey} />
-        </group>
-        <group name="table" position={[0.071, -0.538, -0.354]} rotation={[0, 1.571, 0]} scale={[1, 1, 0.932]}>
-          <mesh name="Cube" geometry={nodes.Cube.geometry} material={materials.brown_wood} />
-          <mesh name="Cube_1" geometry={nodes.Cube_1.geometry} material={materials.white_wood} />
-          <mesh name="Cube_2" geometry={nodes.Cube_2.geometry} material={materials.bronze} />
+          <mesh name="Plane015" geometry={nodes.Plane015.geometry} material={materials.rough_plastic_beige} />
+          <mesh name="Plane015_1" geometry={nodes.Plane015_1.geometry} material={materials.rough_plastic_dark_beige} />
+          <mesh name="Plane015_2" geometry={nodes.Plane015_2.geometry} material={materials.rough_plastic_GREY} />
+          <mesh name="Plane015_3" geometry={nodes.Plane015_3.geometry} material={materials.metalic_light_grey} />
         </group>
         <group name="table_pens" position={[0.212, -0.155, -0.322]} rotation={[0, 1.571, 0]}>
           <mesh name="Cone008" geometry={nodes.Cone008.geometry} material={materials.pencil} />
@@ -289,16 +265,73 @@ export function World(props) {
           <mesh name="Cube005_4" geometry={nodes.Cube005_4.geometry} material={materials.metroid} />
           <mesh name="Cube005_5" geometry={nodes.Cube005_5.geometry} material={materials.zelda_snes} />
         </group>
-        <group name="VCR001" position={[-0.466, -0.144, -0.403]} rotation={[0, -1.571, 0]} scale={[0.056, 0.383, 0.466]}>
-          <mesh name="Cube006" geometry={nodes.Cube006.geometry} material={materials.smooth_plastic_black} />
-          <mesh name="Cube006_1" geometry={nodes.Cube006_1.geometry} material={materials.smooth_plastic_grey} />
-          <mesh name="Cube006_2" geometry={nodes.Cube006_2.geometry} material={materials.arch_glass} />
-          <mesh name="Cube006_3" geometry={nodes.Cube006_3.geometry} material={materials.white} />
-        </group>
         <group name="cup" position={[0.724, -0.176, -0.199]} rotation={[0, 1.571, 0]} scale={[1, 1, 0.932]}>
           <mesh name="Cube007" geometry={nodes.Cube007.geometry} material={materials.white} />
           <mesh name="Cube007_1" geometry={nodes.Cube007_1.geometry} material={materials.smooth_plastic_black} />
         </group>
+        <group name="VCR002" position={[-0.641, -0.143, -0.569]}>
+          <mesh name="Cube008" geometry={nodes.Cube008.geometry} material={materials.smooth_plastic_black} />
+          <mesh name="Cube008_1" geometry={nodes.Cube008_1.geometry} material={materials.smooth_plastic_grey} />
+          <mesh name="Cube008_2" geometry={nodes.Cube008_2.geometry} material={materials.arch_glass} />
+          <mesh name="Cube008_3" geometry={nodes.Cube008_3.geometry} material={materials.white} />
+        </group>
+        <group name="VCR003" position={[-0.667, -0.143, -0.569]}>
+          <mesh name="Cube008" geometry={nodes.Cube008.geometry} material={materials.smooth_plastic_black} />
+          <mesh name="Cube008_1" geometry={nodes.Cube008_1.geometry} material={materials.smooth_plastic_grey} />
+          <mesh name="Cube008_2" geometry={nodes.Cube008_2.geometry} material={materials.arch_glass} />
+          <mesh name="Cube008_3" geometry={nodes.Cube008_3.geometry} material={materials.white} />
+        </group>
+        <group name="VCR004" position={[-0.462, -0.132, -0.412]} rotation={[0, -0.381, Math.PI / 2]}>
+          <mesh name="Cube008" geometry={nodes.Cube008.geometry} material={materials.smooth_plastic_black} />
+          <mesh name="Cube008_1" geometry={nodes.Cube008_1.geometry} material={materials.smooth_plastic_grey} />
+          <mesh name="Cube008_2" geometry={nodes.Cube008_2.geometry} material={materials.arch_glass} />
+          <mesh name="Cube008_3" geometry={nodes.Cube008_3.geometry} material={materials.white} />
+        </group>
+        <group name="VCR005" position={[-0.294, -0.143, -0.569]}>
+          <mesh name="Cube008" geometry={nodes.Cube008.geometry} material={materials.smooth_plastic_black} />
+          <mesh name="Cube008_1" geometry={nodes.Cube008_1.geometry} material={materials.smooth_plastic_grey} />
+          <mesh name="Cube008_2" geometry={nodes.Cube008_2.geometry} material={materials.arch_glass} />
+          <mesh name="Cube008_3" geometry={nodes.Cube008_3.geometry} material={materials.white} />
+        </group>
+        <group name="VCR006" position={[-0.27, -0.143, -0.569]}>
+          <mesh name="Cube008" geometry={nodes.Cube008.geometry} material={materials.smooth_plastic_black} />
+          <mesh name="Cube008_1" geometry={nodes.Cube008_1.geometry} material={materials.smooth_plastic_grey} />
+          <mesh name="Cube008_2" geometry={nodes.Cube008_2.geometry} material={materials.arch_glass} />
+          <mesh name="Cube008_3" geometry={nodes.Cube008_3.geometry} material={materials.white} />
+        </group>
+        <group name="VCR007" position={[-0.429, -0.198, -0.259]} rotation={[0, 0.345, Math.PI / 2]}>
+          <mesh name="Cube008" geometry={nodes.Cube008.geometry} material={materials.smooth_plastic_black} />
+          <mesh name="Cube008_1" geometry={nodes.Cube008_1.geometry} material={materials.smooth_plastic_grey} />
+          <mesh name="Cube008_2" geometry={nodes.Cube008_2.geometry} material={materials.arch_glass} />
+          <mesh name="Cube008_3" geometry={nodes.Cube008_3.geometry} material={materials.white} />
+        </group>
+        <group name="VCR008" position={[-0.295, -0.151, -0.4]} rotation={[0, 0, 0.529]}>
+          <mesh name="Cube008" geometry={nodes.Cube008.geometry} material={materials.smooth_plastic_black} />
+          <mesh name="Cube008_1" geometry={nodes.Cube008_1.geometry} material={materials.smooth_plastic_grey} />
+          <mesh name="Cube008_2" geometry={nodes.Cube008_2.geometry} material={materials.arch_glass} />
+          <mesh name="Cube008_3" geometry={nodes.Cube008_3.geometry} material={materials.white} />
+        </group>
+        <mesh name="posters001" geometry={nodes.posters001.geometry} material={materials.final_fantasy} position={[0.593, 0.204, -0.673]} rotation={[Math.PI / 2, 0, -Math.PI / 2]} scale={[0.168, 1, 0.233]} />
+        <mesh name="posters002" geometry={nodes.posters002.geometry} material={materials.mega_man} position={[0.301, 0.217, -0.673]} rotation={[Math.PI / 2, 0, -Math.PI / 2]} scale={[0.168, 1, 0.233]} />
+        <mesh name="posters003" geometry={nodes.posters003.geometry} material={materials.monkey_island} position={[-0.057, 0.216, -0.673]} rotation={[Math.PI / 2, 0, -Math.PI / 2]} scale={[0.168, 1, 0.233]} />
+        <mesh name="posters004" geometry={nodes.posters004.geometry} material={materials.b2f} position={[-0.493, 0.235, -0.673]} rotation={[Math.PI / 2, 0, -Math.PI / 2]} scale={[0.168, 1, 0.233]} />
+        <mesh name="posters005" geometry={nodes.posters005.geometry} material={materials.et} position={[-0.678, 0.175, -0.287]} rotation={[Math.PI / 2, 0, -Math.PI / 2]} scale={[0.168, 1, 0.233]} />
+        <group name="book_shelves001" position={[0.014, 0.092, -0.608]} rotation={[0, 1.571, 0]}>
+          <mesh name="Cube009" geometry={nodes.Cube009.geometry} material={materials.brown_wood} />
+          <mesh name="Cube009_1" geometry={nodes.Cube009_1.geometry} material={materials.white_wood} />
+        </group>
+        <mesh name="walls&floor002" geometry={nodes['walls&floor002'].geometry} material={materials.carpet} position={[0, -0.476, 0]} rotation={[0, 1.571, 0]} />
+        <mesh name="walls&floor003" geometry={nodes['walls&floor003'].geometry} material={materials.window_frame} position={[-0.75, 0.132, 0.458]} rotation={[0, 1.571, 0]} />
+        <mesh name="book_shelves002" geometry={nodes.book_shelves002.geometry} material={materials.bronze} position={[0.076, -0.546, -0.045]} rotation={[0, 1.571, 0]} />
+        <group name="pc&floppies001" position={[0.115, 0.014, -0.371]}>
+          <mesh name="Plane014" geometry={nodes.Plane014.geometry} material={materials.white_striped_paper} />
+          <mesh name="Plane014_1" geometry={nodes.Plane014_1.geometry} material={materials.floppy_disk_black} />
+          <mesh name="Plane014_2" geometry={nodes.Plane014_2.geometry} material={materials.silver_metal} />
+          <mesh name="Plane014_3" geometry={nodes.Plane014_3.geometry} material={materials.floppy_disk_blue} />
+        </group>
+        <mesh name="pc&floppies002" geometry={nodes['pc&floppies002'].geometry} material={materials.green_emission} position={[0.115, 0.014, -0.371]} />
+        <mesh name="avatar002" geometry={nodes.avatar002.geometry} material={materials['black tshirt']} position={[-0.504, -0.228, 0.546]} rotation={[-0.049, 0.408, 0.277]} scale={[1, 0.942, 1]} />
+        <mesh name="avatar003" geometry={nodes.avatar003.geometry} material={materials.grey_dark} position={[-0.504, -0.228, 0.546]} rotation={[-0.049, 0.408, 0.277]} scale={[1, 0.942, 1]} />
         <mesh name="fire_logs" geometry={nodes.fire_logs.geometry} material={materials.fire_log} position={[-1.104, -0.86, 1.344]} rotation={[-1.563, 0.204, -1.608]} scale={[0.057, 0.245, 0.051]} />
         <mesh name="flame_red" geometry={nodes.flame_red.geometry} material={nodes.flame_red.material} position={[-1.174, -0.723, 1.38]} scale={0.413} />
         <mesh name="flame_white" geometry={nodes.flame_white.geometry} material={nodes.flame_white.material} position={[-1.146, -0.651, 1.365]} rotation={[0.244, 0, 0]} scale={0.699} />
