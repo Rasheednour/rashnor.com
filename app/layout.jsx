5eea07ba9d8fddf45549d3b147d577176c8f3972
Rasheednour <rasheednour@gmail.com>
2023-08-11 14:25:38 +0200

    FEAT: Basic Next app setup

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "rashnor.com",
+  description: "Full Stack Developer Portfolio",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+          rel="stylesheet"
+        ></link>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "@styles/globals.css";
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className='font-montserrat'>
+            Welcome to the home page
+        </h1>
+    </div>
+  )
+}
+
+export default Home
